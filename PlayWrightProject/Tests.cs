54dbece4b18f862701c03185cabc236f40cf6b08
oleksandr.kolesnyk <alexander.kolesnik.95@gmail.com>
2023-05-11 17:45:40 +0300

    SetUp and TearDown env variable

--- PlayWrightProject/Tests.cs
+++ PlayWrightProject/Tests.cs
@@ -7,16 +7,19 @@ namespace PlaywrightTests;
 [TestFixture]
 public class Tests
 {
+    [OneTimeSetUp]
+    public void OneTimeSetUp() => Environment.SetEnvironmentVariable("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub");
+
     [Test]
     public async Task Test()
     {
-        Environment.SetEnvironmentVariable("SELENIUM_REMOTE_URL", 
-        "http://localhost:4444/wd/hub");
-
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{ Headless = false });
         var page = await browser.NewPageAsync();
         await page.GotoAsync("https://www.heroeswm.ru/");
-        Thread.Sleep(1000);
+        Thread.Sleep(1000);       
     }
+
+    [OneTimeTearDown]
+    public void TearDown() => Environment.SetEnvironmentVariable("SELENIUM_REMOTE_URL", null);
 }
