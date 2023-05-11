3ab9679faeac425ddfc8c957af2f00025ede4572
oleksandr.kolesnyk <alexander.kolesnik.95@gmail.com>
2023-05-11 08:29:20 +0300

    Solution Init

---
+++ PlayWrightProject/Tests.cs
@@ -0,0 +1,22 @@
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public async Task Test()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://playwright.dev");
+        Thread.Sleep(10000);
+    }
+}
