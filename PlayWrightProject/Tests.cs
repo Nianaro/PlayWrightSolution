43ca7711af79fa3dbe73de7a477d1fde7cbd373c
oleksandr.kolesnyk <alexander.kolesnik.95@gmail.com>
2023-05-12 17:42:20 +0300

    Go to work page

--- PlayWrightProject/Tests.cs
+++ PlayWrightProject/Tests.cs
@@ -11,13 +11,29 @@ public class Tests : TestBase
     public async Task Test()
     {
         using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{ Headless = false });
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
         var page = await browser.NewPageAsync();
+
         await page.GotoAsync("https://www.heroeswm.ru/");
+        
         await page.Locator("[name=login]").FillAsync("");
         await page.Locator("[name=pass]").FillAsync("");
         await page.Locator("div.entergame").ClickAsync();
-        Thread.Sleep(2000);
+        
+        await page.Locator("#MenuMap").ClickAsync();
+        await page.WaitForLoadStateAsync();
+        
+        var arrows = await page.QuerySelectorAllAsync("#hwm_map_objects_and_buttons .map_obj_table_hover td:nth-child(5)");
+        foreach(var arrow in arrows)
+        {
+            var text = await arrow.InnerTextAsync();
+            if (text.Contains("»»»"))
+            {
+                await arrow.ClickAsync();
+                return;
+            }
+        }
+        await page.WaitForLoadStateAsync();
     }
 
     /* [Test]
